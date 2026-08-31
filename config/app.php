239b417c59b75e2bcf982454e553a988397b2c2e
WPEssential <wordpress.essential@gmail.com>
2026-09-01 02:03:52 +0500

    TASK-0016: register ConnectorServiceProvider in config/app.php

--- config/app.php
+++ config/app.php
@@ -16,4 +16,19 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
+
+    /*
+     |--------------------------------------------------------------------------
+     | Service Providers
+     |--------------------------------------------------------------------------
+     |
+     | The service providers listed here will be automatically loaded on the
+     | request to your application. Feel free to add your own services to
+     | this array to grant expanded functionality to your applications.
+     |
+     */
+    'providers' => [
+        // Connector service bindings (TASK-0016)
+        App\Providers\ConnectorServiceProvider::class,
+    ],
 ];

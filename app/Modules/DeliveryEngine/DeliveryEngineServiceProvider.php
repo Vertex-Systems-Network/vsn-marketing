642e934c685057fbe4ce128c0cb641d1ef73f82f
WPEssential <wordpress.essential@gmail.com>
2026-09-08 16:51:16 +0500

    feat(delivery): bind admission repository

--- app/Modules/DeliveryEngine/DeliveryEngineServiceProvider.php
+++ app/Modules/DeliveryEngine/DeliveryEngineServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace App\Modules\DeliveryEngine;
 
+use App\Modules\DeliveryEngine\Domain\Contracts\DeliveryAdmissionRepository;
 use App\Modules\DeliveryEngine\Domain\Contracts\DeliveryOperationRepository;
 use App\Modules\DeliveryEngine\Domain\Contracts\DeliveryRepository;
 use App\Modules\DeliveryEngine\Domain\Contracts\DeliveryTransaction;
 use App\Modules\DeliveryEngine\Domain\Contracts\RecipientSource;
+use App\Modules\DeliveryEngine\Infrastructure\DatabaseDeliveryAdmissionRepository;
 use App\Modules\DeliveryEngine\Infrastructure\DatabaseDeliveryOperationRepository;
 use App\Modules\DeliveryEngine\Infrastructure\DatabaseDeliveryRepository;
 use App\Modules\DeliveryEngine\Infrastructure\DatabaseDeliveryTransaction;
@@ -18,6 +20,7 @@ final class DeliveryEngineServiceProvider extends ServiceProvider
     {
         $this->app->singleton(DeliveryRepository::class, DatabaseDeliveryRepository::class);
         $this->app->singleton(DeliveryOperationRepository::class, DatabaseDeliveryOperationRepository::class);
+        $this->app->singleton(DeliveryAdmissionRepository::class, DatabaseDeliveryAdmissionRepository::class);
         $this->app->singleton(RecipientSource::class, DatabaseRecipientSource::class);
         $this->app->singleton(DeliveryTransaction::class, DatabaseDeliveryTransaction::class);
     }
